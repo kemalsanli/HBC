795b9bb8c7cc29a8ddfba700c69290e4a49f5073
Kemal Sanlı <kemalsanli@users.noreply.github.com>
2025-11-22 04:11:55 +0300

    Init

---
+++ HandBrake Batch Compressor/HandBrake Batch Compressor/HandBrake_Batch_CompressorApp.swift
@@ -0,0 +1,18 @@
+//
+//  HandBrake_Batch_CompressorApp.swift
+//  HandBrake Batch Compressor
+//
+//  Created by Kemal Sanli on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct VideoShrinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.titleBar)
+    }
+}
